80f44615b15aa2234e43ac93458099e61c6e017a
Gabriel Wojcik <gabrielwojcikv@gmail.com>
2021-02-26 16:42:00 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+var entidade = document.getElementById('cat');
+
+var fato_lupa = 2;
+entidade.onmouseover = function (){
+    this.style.width = (this.clientWidth * fato_lupa) + "px";
+}
+
+entidade.onmouseover = function(){
+    this.style.width = (this.clientWidth / 2) + "px";
+}
+*/
+
